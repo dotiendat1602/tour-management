97aeb2e1084cf7063d91847b04454ba5178afe75
Do Tien Dat <dotiendat711@gmail.com>
2024-10-13 10:14:50 +0700

    Làm trang danh mục Tour (bên client)

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,6 +1,11 @@
 import { Express } from "express";
 import { tourRoute } from "./tour.route";
+import  { categoryRoute } from "./category.route";
+
 
 export const routesClient = (app: Express) => {
     app.use("/tours", tourRoute);
+
+    app.use("/categories",  categoryRoute);
+
 }

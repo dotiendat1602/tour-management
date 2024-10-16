a2ebe7bdcfbdcb9eb70dc94196e3336b84aea8f7
Do Tien Dat <dotiendat711@gmail.com>
2024-10-16 10:46:44 +0700

    Tạo giao diện giỏ hàng và đặt tour

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,11 +1,14 @@
 import { Express } from "express";
 import { tourRoute } from "./tour.route";
 import  { categoryRoute } from "./category.route";
+import { cartRoute } from "./cart.route";
 
 
 export const routesClient = (app: Express) => {
     app.use("/tours", tourRoute);
 
     app.use("/categories",  categoryRoute);
+
+    app.use("/cart", cartRoute);
 
 }

76037f3a4de7b7398d4a70aea7cc4b348f80342f
Do Tien Dat <dotiendat711@gmail.com>
2024-10-16 10:32:41 +0700

    Làm nút Mini Cart trên Header

--- public/js/script.js
+++ public/js/script.js
@@ -23,6 +23,17 @@ const alertAddCartSusscess = () => {
 }
 // End alert-add-cart-susscess
 
+// Hiển thị số lượng sản phẩm vào mini cart
+const showMiniCart = ()  => {
+  const miniCart = document.querySelector("[mini-cart]");
+  if(miniCart) {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    miniCart.innerHTML = cart.length;
+  }
+}
+showMiniCart();
+// Hết Hiển thị số lượng sản phẩm vào mini cart
+
 // Giỏ hàng
 const cart = localStorage.getItem("cart");
 
@@ -54,7 +65,10 @@ if(formAddToCart) {
       }
       
       localStorage.setItem("cart", JSON.stringify(cart));
+
       alertAddCartSusscess();
+      
+      showMiniCart();
     }
   })
 }

8f7f43f72250c59c38b2c846ac5088ef0abd656e
Do Tien Dat <dotiendat711@gmail.com>
2024-10-16 10:25:44 +0700

    Hiển thị thông báo Đã thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -10,6 +10,19 @@ if(tourImages) {
 }
 // End tour-images
 
+// alert-add-cart-susscess
+const alertAddCartSusscess = () => {
+  const elementAlert = document.querySelector("[alert-add-cart-susscess]");
+  if(elementAlert) {
+    elementAlert.classList.remove("alert-hidden");
+
+    setTimeout(() => {
+      elementAlert.classList.add("alert-hidden");
+    }, 3000);
+  }
+}
+// End alert-add-cart-susscess
+
 // Giỏ hàng
 const cart = localStorage.getItem("cart");
 
@@ -41,6 +54,7 @@ if(formAddToCart) {
       }
       
       localStorage.setItem("cart", JSON.stringify(cart));
+      alertAddCartSusscess();
     }
   })
 }

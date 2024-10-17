d056b8f7129f1c2331a744a1bf1f7fd26e90140b
Do Tien Dat <dotiendat711@gmail.com>
2024-10-17 16:32:00 +0700

    Lấy thông tin đơn hàng gửi lên server

--- public/js/script.js
+++ public/js/script.js
@@ -163,3 +163,35 @@ if(tableCart) {
     })
 }
 // Hết Vẽ tour vào giỏ hàng
+
+// Đặt tour
+const formOrder = document.querySelector("[form-order]");
+if(formOrder) {
+  formOrder.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const cart = JSON.parse(localStorage.getItem("cart"));
+
+    const dataFinal = {
+      info: {
+        fullName: formOrder.fullName,
+        phone: formOrder.phone,
+        note: formOrder.note,
+      },
+      cart: cart,
+    };
+
+    fetch("/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(dataFinal)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+  })
+}
+// Hết Đặt tour
--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { tourRoute } from "./tour.route";
 import  { categoryRoute } from "./category.route";
 import { cartRoute } from "./cart.route";
+import { orderRoute } from "./order.route";
 
 
 export const routesClient = (app: Express) => {
@@ -11,4 +12,5 @@ export const routesClient = (app: Express) => {
 
     app.use("/cart", cartRoute);
 
+    app.use("/order", orderRoute);
 }
